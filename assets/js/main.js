08d5d24d76db4f0406db7f9ce24cbb60a8e914ca
Vazgen Ayvazyan <vazgen.ayvazyan7@gmail.com>
2024-02-29 23:01:10 +0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,56 @@
+
+$(document).ready(function () {
+
+    $('.menu-burger').click(function () {
+        $('.header').toggleClass('open-burger');
+        $('body').addClass("visible");
+
+    });
+    $('.nav-close-icon').click(function () {
+        $('.header').removeClass('open-burger');
+        $('body').removeClass("visible");
+
+    });
+    $('.search').on('click', function () {
+        $('.nav').addClass("hidden");
+        $('.header-search-block').addClass("show");
+        // $('body').addClass("visible");
+        $('.form-search-block').addClass("open");
+
+    });
+    $('.search-mb').on('click', function () {
+
+        $('body').addClass("visible");
+        $('.header-search-mobile-block').addClass("open-search-block");
+
+    });
+    $('.search-button-close').on('click', function () {
+        $('.nav').removeClass("hidden");
+        $('.header-search-block').removeClass("show");
+        $('body').removeClass("visible");
+        $('.form-search-block').removeClass("open");
+
+    });
+    $('.mb-search-button-close').on('click', function () {
+
+        $('.header-search-block').removeClass("show");
+        $('body').removeClass("visible");
+        $('.form-search-block').removeClass("open");
+        $('.header-search-mobile-block').removeClass("open-search-block");
+
+
+    });
+
+
+    ;
+
+
+
+
+
+
+
+
+
+
+});
